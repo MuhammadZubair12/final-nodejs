e96399af0bdb21cb3567de9a647aa800fb71935d
MuhammadZubair12 <zubairmazhar23@gmail.com>
2021-09-23 20:20:45 +0500

    a

--- api/models/company.js
+++ api/models/company.js
@@ -1,31 +1,37 @@
 
-module.exports = (sequelize, Sequelize) => {
+const Sequelize = require('sequelize');
+const bcryptService = require('../services/bcrypt.service');
 
-    var Company = sequelize.define('company', {
-        id: {
-            autoIncrement: true,
-            primaryKey: true,
-            type: Sequelize.INTEGER,
-        },
-        company_name: {
-            type: Sequelize.STRING,
-        },
-        email: {
-            type: Sequelize.STRING,
-        },
-        longitude: {
-            type: Sequelize.STRING,
-        },
-        latitude: {
-            type: Sequelize.STRING,
-        },
-        phone_number: {
-            type: Sequelize.STRING,
-        },
-        status: {
-            type: Sequelize.STRING,
-        }
-    });
+const sequelize = require('../../config/database');
 
-    return Company;
-}
+const hooks = {};
+
+const tableName = 'companies';
+
+const Company = sequelize.define('Company', {
+    id: {
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+    },
+    company_name: {
+        type: Sequelize.STRING,
+    },
+    email: {
+        type: Sequelize.STRING,
+    },
+    longitude: {
+        type: Sequelize.STRING,
+    },
+    latitude: {
+        type: Sequelize.STRING,
+    },
+    phone_number: {
+        type: Sequelize.STRING,
+    },
+    status: {
+        type: Sequelize.STRING,
+    }
+}, { hooks, tableName });
+
+module.exports = Company;
