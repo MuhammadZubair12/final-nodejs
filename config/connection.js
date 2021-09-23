3421ca3a56991a7759a6ba54156abea8fe071546
MuhammadZubair12 <zubairmazhar23@gmail.com>
2021-09-23 19:05:12 +0500

    databse credentials

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,17 @@
 const development = {
-  database: 'final',
-  username: 'postgres',
-  password: 'c00822631',
-  host: 'localhost',
+  database: 'daok5s3mq2qivs',
+  username: 'otgrnegltiqonb',
+  password: '0eb759d3c69f5b9da3f7c4a825cc97c7b2889e790bb8329aa8ca9ad54d4deed6',
+  host: 'ec2-44-199-83-229.compute-1.amazonaws.com',
   dialect: 'postgres',
 };
 
 const testing = {
-  database: 'databasename',
-  username: 'username',
-  password: 'password',
-  host: 'localhost',
-  dialect: 'sqlite' || 'mysql' || 'postgres',
+  database: 'daok5s3mq2qivs',
+  username: 'otgrnegltiqonb',
+  password: '0eb759d3c69f5b9da3f7c4a825cc97c7b2889e790bb8329aa8ca9ad54d4deed6',
+  host: 'ec2-44-199-83-229.compute-1.amazonaws.com',
+  dialect: 'postgres',
 };
 
 const production = {
@@ -19,7 +19,7 @@ const production = {
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   host: process.env.DB_HOST || 'localhost',
-  dialect: 'sqlite' || 'mysql' || 'postgres',
+  dialect:'postgres',
 };
 
 module.exports = {
