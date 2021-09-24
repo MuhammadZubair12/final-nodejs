380403e5579d633edef64e5249fb99acae22e175
MuhammadZubair12 <zubairmazhar23@gmail.com>
2021-09-24 17:00:29 +0500

    a

--- config/routes/publicRoutes.js
+++ config/routes/publicRoutes.js
@@ -2,9 +2,7 @@ const publicRoutes = {
   'POST /register': 'UserController.register', // alias for POST /user
   'POST /login': 'UserController.login',
   'POST /validate': 'UserController.validate',
-  // 'POST /company': 'UserController.company',
-  // 'POST /assets': 'UserController.assets',
-  // 'POST /abc': 'UserController.abc',
+  'POST /abc': 'AbcController.abc',
 };
 
 module.exports = publicRoutes;
